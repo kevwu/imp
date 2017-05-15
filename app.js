da2666612c9077dff612c245a83750e32ae645ce
Kevin Wu <kevin@kevinywu.com>
2017-05-15 15:20:25 -0500

    Add control over top row buttons

--- app.js
+++ app.js
@@ -3,20 +3,53 @@ let WebMidi = require("webmidi")
 
 let Launchpad = {}
 
-Launchpad.padOn = function(row, col, color) {
-	this.output.playNote((10 * row) + col, 1, {
-		rawVelocity: true,
-		velocity: color
-	})
-}
+Launchpad.setPad = function(row, col, state, color=0) {
+	if(row > 9 || row < 1 || col > 9 || col < 1) {
+		console.log("Invalid row/col: " + row + "," + col)
+		return
+	}
+	// the Launchpad uses three channels for different states of lights
+	let channel = 0
 
-Launchpad.padOff = function(row, col) {
-	this.output.stopNote((10*row) + col)
+	switch(state) {
+		case "on":
+			channel = 1;
+			break;
+		case "flash":
+			channel = 2;
+			break;
+		case "pulse":
+			channel = 3;
+			break;
+		case "off":
+			channel = "all"
+			color = 0;
+			break;
+		default:
+			console.log("Invalid state: " + state)
+			return
+	}
+
+	// the top row of round buttons is handled differently
+	if(row === 9) {
+		Launchpad.output.sendControlChange(col + 103, color, channel)
+	} else {
+		let note = (10 * row) + col;
+		this.output.playNote((10 * row) + col, channel, {
+			rawVelocity: true,
+			velocity: color
+		})
+	}
 }
 
 Launchpad.clearAll = function() {
 	for(let i = 0; i < 128; i += 1) {
 		Launchpad.output.stopNote(i)
+	}
+
+	// clear top buttons
+	for(let i = 104; i <= 111; i += 1) {
+		Launchpad.output.sendControlChange(i, 0, "all")
 	}
 }
 
@@ -32,13 +65,6 @@ WebMidi.enable((err) => {
 		console.log(Launchpad)
 
 		Launchpad.clearAll()
-		// for(let i = 1; i <= 8; i += 1){
-		// 	for(let j = 1; j <= 8; j += 1){
-		// 		Launchpad.padOn(i, j, (8 * i) + j)
-		// 	}
-		// }
-
-		Launchpad.clearAll()
 
 		Tone.Transport.bpm.value = 60
 
@@ -50,14 +76,14 @@ WebMidi.enable((err) => {
 				metroPos = 1
 			}
 			console.log(metroPos)
-			Launchpad.padOff(metroPos > 1 ? metroPos - 1 : 8, 9, 3)
-			Launchpad.padOn(metroPos, 9, 3)
+			Launchpad.setPad(metroPos > 1 ? metroPos - 1 : 8, 9, "off", 3)
+			Launchpad.setPad(metroPos, 9, "on", 3)
 		}, "8n")
 
 		// Tone.Transport.scheduleRepeat((time) => {
-		// 	for(let i = 1; i <= 8; i += 1){
-		// 		for(let j = 1; j <= 8; j += 1){
-		// 			Launchpad.padOn(i, j, Math.floor(Math.random()*(127-1+1)+1))
+		// 	for(let i = 1; i <= 9; i += 1){
+		// 		for(let j = 1; j <= 9; j += 1){
+		// 			Launchpad.setPad(i, j, "on", Math.floor(Math.random()*(127-1+1)+1))
 		// 		}
 		// 	}
 		// }, "8n")
@@ -65,14 +91,7 @@ WebMidi.enable((err) => {
 		Tone.Transport.start()
 
 		let sequence = new SequencePattern()
-
-		Launchpad.input.on("noteon", "all", (event) => {
-			let column =  event.note.number % 10
-			let row = parseInt(event.note.number / 10)
-
-			// root note: A4
-			sequence.addNote(Tone.Frequency("A4").transpose(row - 1), column)
-		})
+		sequence.activate()
 	}
 }, true)
 
@@ -85,11 +104,19 @@ class Pattern {
 		this.instrument = null
 	}
 
+	activate() {
+
+	}
+
 	addNote(note, position) {
 
 	}
 
 	removeNote(note, position) {
+
+	}
+
+	toggleNote(note, position) {
 
 	}
 }
@@ -113,7 +140,17 @@ class SequencePattern extends Pattern{
 		// TODO allow for arbitrary length patterns
 	}
 
-	addNote(note, position) {
+	activate() {
+		Launchpad.input.on("noteon", "all", (event) => {
+			let column =  event.note.number % 10
+			let row = parseInt(event.note.number / 10)
+
+			// root note: A4
+			sequence.toggleNote(Tone.Frequency("A4").transpose(row - 1), column)
+		})
+	}
+
+	toggleNote(note, position) {
 		if(typeof this.sequence[position] === "undefined") {
 			this.sequence[position] = {}
 		}
@@ -121,13 +158,9 @@ class SequencePattern extends Pattern{
 
 		let noteRow = note.toMidi() - Tone.Frequency("A4").toMidi() + 1
 
-		Launchpad.padOn(noteRow, position, 45)
+		Launchpad.setPad(noteRow, position, "pulse", 45)
 
 		console.log(position)
 		console.log(note)
-	}
-
-	removeNote(note, position) {
-
 	}
 }
